8089b70c6b6c97a7e0a3d7cc68798e430c73ea01
ryandelmar <30875224+ryandelmar@users.noreply.github.com>
2017-08-30 08:36:07 -1000

    Create arrays in objects.js

---
+++ arrays in objects.js
@@ -0,0 +1,7 @@
+var ryan = {firstname: "Ryan", lastname: "Martinez", age: 16, grade: 12, friends:["kiva", "Chris", "Avi"]};
+var kiva = {firstname: "Kiva", lastname: "Nguyen", age: 17, grade: 12, friends:["Ryan", "Raen", "Daniel"]};
+var avi = {firstname: "Avi", lastname: "Laird", age: 17, grade: 12, friends:["Colt", "Ryan", "Mike"]};
+
+var x = [ryan,kiva,avi];
+
+window.alert(x[1].friends[0]);
